3a92e6de91cddbace9b9f39735d657be64b0e2cc
Emil Thorenfeldt <emt@magenta-aps.dk>
2022-09-09 09:27:09 +0200

    Add very basic error handling

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,17 @@ use url::Url;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_config()?;
     let redmine = Redmine::new(Url::parse(&config.redmine_url)?, &config.redmine_api_key)?;
+    if let Ok(issues) = query_issues(&redmine) {
+        println!("{} issues assigned", issues.len());
+    } else {
+        eprintln!("Could not connect to Redmine");
+    }
+    Ok(())
+}
+
+fn query_issues(redmine: &Redmine) -> Result<Vec<IssueEssentials>, Box<dyn std::error::Error>> {
     let endpoint = ListIssues::builder().assignee(AssigneeFilter::Me).build()?;
     let IssuesWrapper { issues } =
         redmine.json_response_body::<_, IssuesWrapper<IssueEssentials>>(&endpoint)?;
-    println!("{} issues assigned", issues.len());
-    Ok(())
+    Ok(issues)
 }
